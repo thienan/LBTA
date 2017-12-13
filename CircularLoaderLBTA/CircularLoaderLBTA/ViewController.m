868b0d121913c6ea9c94a657a6d18c5b1be822d4
Ihar Tsimafeichyk <igarekas@gmail.com>
2017-12-13 13:46:26 +0300

    Updated UI

--- CircularLoaderLBTA/CircularLoaderLBTA/ViewController.m
+++ CircularLoaderLBTA/CircularLoaderLBTA/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()<NSURLSessionDownloadDelegate>
 @property (strong, nonatomic) CAShapeLayer *shapeLayer;
 @property (strong, nonatomic) UILabel *percentageLabel;
+@property (strong, nonatomic) UILabel *infoLabel;
+
 
 @end
 
@@ -21,6 +23,8 @@
     
     [self.view setBackgroundColor:[UIColor blackColor]];
 
+    CGFloat lineWidth = 20.0f;
+    
     UIBezierPath *circuitPath = [UIBezierPath
                                  bezierPathWithArcCenter:CGPointZero
                                  radius:100 startAngle:0
@@ -29,15 +33,18 @@
     CAShapeLayer *trackLayer = [[CAShapeLayer alloc] init];
     trackLayer.strokeColor = [[UIColor lightGrayColor] CGColor];
     trackLayer.path = circuitPath.CGPath;
-    trackLayer.lineWidth = 10;
+    trackLayer.lineWidth = lineWidth;
     trackLayer.fillColor = [[UIColor clearColor] CGColor];
     trackLayer.position = self.view.center;
     [self.view.layer addSublayer:trackLayer];
 
     self.shapeLayer = [[CAShapeLayer alloc] init];
     self.shapeLayer.path = circuitPath.CGPath;
-    self.shapeLayer.strokeColor = [[UIColor redColor] CGColor];
-    self.shapeLayer.lineWidth = 10;
+    self.shapeLayer.strokeColor = [[UIColor colorWithRed:232.0f/255.0f
+                                                   green:43.0f/255.0f
+                                                    blue:109.0f/255.0f
+                                                   alpha:1] CGColor];
+    self.shapeLayer.lineWidth = lineWidth;
     self.shapeLayer.strokeEnd = 0;
     self.shapeLayer.lineCap = kCALineCapRound;
     self.shapeLayer.fillColor = [[UIColor clearColor] CGColor];
@@ -45,23 +52,36 @@
     self.shapeLayer.transform = CATransform3DMakeRotation(-M_PI/2, 0, 0, 1);
     [self.view.layer addSublayer:self.shapeLayer];
 
-    UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap)];
+    UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]
+                                       initWithTarget:self action:@selector(handleTap)];
     [self.view addGestureRecognizer:tapGesture];
 
-    [self updateLabel];
+    [self updateLabels];
 }
 
 #pragma mark - Private methods
 
-- (void) updateLabel {
+- (void) updateLabels {
     self.percentageLabel = [[UILabel alloc] init];
     [self.percentageLabel setText:@"Start"];
     [self.percentageLabel setTextAlignment:NSTextAlignmentCenter];
-    [self.percentageLabel setFont:[UIFont boldSystemFontOfSize:32.0]];
-    [self.percentageLabel setTextColor:[UIColor grayColor]];
-    [self.view addSubview:self.percentageLabel];
+    //[self.percentageLabel setFont:[UIFont boldSystemFontOfSize:37.0f]];
+    [self.percentageLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:37.0f]];
+    [self.percentageLabel setTextColor:[UIColor whiteColor]];
     [self.percentageLabel setFrame:CGRectMake(0, 0, 100, 100)];
-    [self.percentageLabel setCenter:self.view.center];
+    [self.percentageLabel setCenter:CGPointMake(self.view.center.x, self.view.center.y - 10)];
+    [self.view addSubview:self.percentageLabel];
+    
+    self.infoLabel = [[UILabel alloc] init];
+    [self.infoLabel setText:@"Completed"];
+    [self.infoLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.infoLabel setTextColor:[UIColor whiteColor]];
+    //[self.infoLabel setFont:[UIFont systemFontOfSize:14.0f]];
+    [self.infoLabel setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:14.0f]];
+    [self.infoLabel setFrame:CGRectMake(0, 0, 100, 100)];
+    [self.infoLabel  setCenter:CGPointMake(self.view.center.x, self.view.center.y + 25)];
+    [self.infoLabel setAlpha:0.0f];
+    [self.view addSubview:self.infoLabel];
 }
 
 - (void) handleTap {
@@ -85,7 +105,9 @@
 - (void) beginDownloadingFile {
     NSLog(@"%@", @"File downloading begins");
     
-    self.shapeLayer.strokeEnd = 0;
+    [self.shapeLayer setStrokeEnd:0.0f];
+    [self.infoLabel setAlpha:0];
+    
 
     //NSString *urlString = @"https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c";
 
@@ -109,6 +131,13 @@
 #pragma mark - NSURLSessionTaskDelegate
 
 - (void)URLSession:(nonnull NSURLSession *)session downloadTask:(nonnull NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(nonnull NSURL *)location {
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (_infoLabel) {
+            [_infoLabel setAlpha:1.0];
+        }
+    });
+
     NSLog(@"%@", @"File downloading end");
 }
 
